11669bdf6c4b9eb71400c330491b6fd3c78f32a3
CANDRGROUP\k.oishi <kouta.oishi455@gmail.com>
2023-01-19 14:35:56 +0900

    スライム追加

---
+++ My project/Assets/Scripts/PlayerAttackController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_AttackCollider;
+    void Update()
+    {
+        if( Input.GetMouseButtonDown( 1 ) )
+        {
+
+        }
+    }
+}
